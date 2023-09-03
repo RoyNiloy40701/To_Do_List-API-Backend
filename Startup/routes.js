90ebb5cef37c3f059b176f1260fcc921e2df5939
Niloy <niloyroyaiub@gmail.com>
2023-09-04 01:32:12 +0600

    first commit

--- Startup/routes.js
+++ Startup/routes.js
@@ -1,13 +1,13 @@
-const tasks=require('../Routes/tasks');
-const users=require('../Routes/users');
-const auth=require('../Routes/auth');
+const tasks = require('../Routes/tasks');
+const users = require('../Routes/users');
+const auth = require('../Routes/auth');
 const express = require('express');
 
 
-module.exports=function(app){
+module.exports = function (app) {
      app.use(express.json());
-     app.use('/api/tasks',tasks);
-     app.use('/api/users',users);
+     app.use('/api/tasks', tasks);
+     app.use('/api/users', users);
      app.use('/api/auth', auth);
 }
-     
+
